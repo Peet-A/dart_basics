92736981fef5ae534dba4e990c3e5974f70a69ce
Peter Aziz <peet.a.91@gmail.com>
2020-04-23 23:29:42 +0200

    Initial commit

---
+++ lib/dart_basics.dart
@@ -0,0 +1,82 @@
+void main() {
+  /// there is some tasks that i need you to work on.
+  /// 1- just print to the console this world "Hello World" .
+  /// 2- just print to the console this world "it's okay" .
+  /// 3- how to write a comment ? write this as a comment :  "this is the main number " .
+  /// 4- print the sum of 2 and 3 .
+  /// 5- what is the difference between "var" and "int" ? .
+  /// 6- what is the difference between "int" and "double" ? .
+  /// 7- use these variables to print the age of same like " Samy is 10 years old "
+//  int age = 10;
+//  String name = "Samy";
+
+  /// 8- print how many items in the list
+//  List values = ["3.2", 233, false, 3.24];
+
+  /// 9- what is the difference between final and const ? .
+  /// 10- use if statement to print "good deal" and assign it to a variable with any name
+  /// if value1 is higher than 132 and value2 is less than 70 and print "bad deal" in other
+  /// deals .
+//  int value1 = 200;
+//  int value2 = 50;
+
+  /// 11- write this code  in another similar way .
+//  if (value1 == 100) {
+//    print("good job");
+//  } else {
+//    print("bad job");
+//  }
+
+  /// 12- what is this "??" and where can we use it ?
+
+  /// 13- use the switch case to deal with these conditions for the value of myAge .
+//  int myAge = null;
+
+  /// a -  if myAge = 15 => print "too young"
+  /// b -  if myAge = 20 => print "too old"
+  /// c -  if myAge = 17 => print "nice"
+  /// and if myAge is null or not set ,  print "waiting..."
+  ///
+
+  /// 14- print "I am Dart developer" for 100 time by 3 different ways ,
+  /// and type the difference between the 3 ways .
+
+  /// 15 - create a function to get 2 numbers and return the sum (use named parameters ) .
+  ///
+  /// 16 - create a function to get 2 numbers and return the sum (use named and required parameters ) .
+  ///
+  /// 17 - create a function to get 2 numbers and return the sum (use named and required parameters )
+  /// with the short hand syntax way or the fat arrow .
+  ///
+  /// 18- what is the void function ?
+  ///
+  /// 19- create a function to get 2 numbers and return the sum (do not use named and required parameters ) .
+  /// and print the maximum number .
+  ///
+  /// 20- create a function with 3 numbers as inputs  and  get 2 required from user and the last one set it's default value to 10
+  ///  and return the sum of the 3 numbers  .
+  /// and print the maximum number .
+  ///
+  /// 21- use the Exception Handling to print the error in   var v = 1 ~/ 0;
+  ///
+  /// 22-  create "store" class with 2 properties name and location and also create a function called "knowMyStore"
+  /// (use named parameters) when creating the constructor .
+  /// to print the store name and the location like this way
+  ///  " My store name is Cleo , it's in Cairo . "
+
+  ///
+
+  /// 23 - create a list of 5 random numbers and get the sum of the list .
+  ///
+  ///
+  /// 24 - create an 10 records array of "values i" and numbers  and get the sum of the numbers .
+  /// like this one
+
+//  var array = {
+//    "value1": 2,
+//    "value2": 3,
+//     ..........
+//
+
+  /// Ends of the test .
+}
